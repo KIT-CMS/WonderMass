12938de9d025dfd186e4b378e50a395af5f5e74c
Olena Hlushchenko <greyxray@gmail.com>
2019-06-12 19:46:18 +0200

    scripts to set training env

---
+++ bin/training/checkout.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+virtualenv py2_venv
+source py2_venv/bin/activate
+which pip
+pip install --upgrade pip
+pip install tensorflow-gpu
+pip install keras
+pip install uproot
+pip install numpy
+pip install matplotlib
+pip install root_pandas
+
+thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $thisdir/setup_env.sh
---
+++ bin/training/setup_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source py2_venv/bin/activate
+
+export PATH=/usr/local/cuda-10.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
+
+export KERAS_BACKEND=tensorflow
+export CUDA_VISIBLE_DEVICES='2'
